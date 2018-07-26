001e82056e5ffaff8fc271980146078aa5324556
Nico Zingarelli <ndzingarelli@gmail.com>
2018-07-26 12:29:07 -0700

    added enum eventtype to migration (had already added to model)

--- migrations/20180710004247-create-contract-event.js
+++ migrations/20180710004247-create-contract-event.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       eventType: {
-        type: Sequelize.STRING
+        type: Sequelize.ENUM("DidVCSettle", "DidLCUpdateState")
       },
       fields: {
         type: Sequelize.JSONB
