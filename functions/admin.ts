82648fe6d7a12d06757b8431b00545d85a515a35
Eli <elihanover@yahoo.com>
2018-08-03 12:22:33 -0700

    change function routing and remove postgres resources

--- functions/admin.ts
+++ functions/admin.ts
@@ -23,7 +23,7 @@ export async function setLastBlock(event, context, callback) {
       }
     }, function(err, data) {
       if (err) console.log(err, err.stack)
-      else console.log(data)
+      else console.log("data: " + data)
     })
 
     console.log("BOOM")
--- migrations.ts
+++
@@ -1,8 +0,0 @@
-const { up, down } = require("serverless-pg-migrations/handlers");
-
-// Undefined as of here...
-console.log(up)
-console.log(down)
-
-module.exports.up = up;
-module.exports.down = down;
